6ecd4eae3b274f5ef36084846cc85b8f193f2b93
Mohit Goyal <mohitgoyal2104@gmail.com>
2014-03-20 15:13:26 +0530

    insertion of elements
    insertion of elements without affecting the features of code mirror.

---
+++ insert.js
@@ -0,0 +1,8 @@
+
+
+
+var height_of_row=16;
+var z=parseInt(document.getElementsByClassName("CodeMirror-cursor")[0].style.top)/height_of_row;
+var l=document.getElementsByClassName("CodeMirror-linenumber")[z].parentNode.parentNode;
+l.appendChild(y);
+
